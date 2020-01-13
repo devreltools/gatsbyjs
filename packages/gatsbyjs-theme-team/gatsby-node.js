482376017ae8e09a5956445d9999cff1e667ccce
David McKay <rawkode@pm.me>
2020-01-13 17:46:47 +0000

    feat: consume DevRelTools Schema and add to GraphQL

--- packages/gatsbyjs-theme-team/gatsby-node.js
+++ packages/gatsbyjs-theme-team/gatsby-node.js
@@ -1,19 +1,8 @@
+import { teamSchema } from "@devreltools/schema";
+
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions;
+  const typeDefs = teamSchema;
 
-  const typeDefs = `
-    type SocialHandles {
-      github: String
-      gitlab: String
-      slack: String
-      twitter: String
-    }
-
-    type TeamMember implements Node @dontInfer {
-      name: String!
-      email: String!
-      handles: SocialHandles
-    }
-  `;
   createTypes(typeDefs);
 };
